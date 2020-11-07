0750b79de2f9ba2ac5e80535d7bb939884a40a7d
Serhii Stanislav <serhiistanislav@gmail.com>
2020-11-08 01:24:28 +0200

    adds some changes

--- js/task-08.js
+++ js/task-08.js
@@ -21,13 +21,22 @@
  * 
  */
 
+// - Находим div и кнопки по data-*
 
+const boxesRef = document.getElementById("boxes");
 const renderBtnRef = document.querySelector('button[data-action="render"]');
 const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
-const boxesRef = document.getElementById("boxes");
+
+// - Запускаем кликом функцию получения значения из инпута (type="number")
+// и функцию очистки строки (удалить контент в input)
 
 renderBtnRef.addEventListener("click", getInputValue);
 destroyBtnRef.addEventListener("click", destroyBoxes);
+
+// 2. - Функция создает div в который впихиваются маленькие дивы 
+// с начальным значением созданные в цикле, рандомных цветов 
+// и динамическим размером. Количество итераций определяется  
+// amount - значением инпута type="number"
 
 function createBoxes(amount) {
   const initialSize = 30;
@@ -41,15 +50,22 @@ function createBoxes(amount) {
   boxesRef.appendChild(containerRef);
 };
 
+// 1. - Функция берет из инпута значение, и в его 
+// контексте запускает создание дивчика 
+
 function getInputValue() {
   const inputValue = document.querySelector("#controls > input").value;
   createBoxes(inputValue);
 };
 
-function destroyBoxes() {
-  boxesRef.textContent = "";
-};
+// 3. - Функция создает рандомные цвета rgba
 
 function randomColor() {
   return Math.floor(Math.random() * 256);
+};
+
+// 4. - Функция очищает строку (инпут)
+
+function destroyBoxes() {
+  boxesRef.textContent = "";
 };
